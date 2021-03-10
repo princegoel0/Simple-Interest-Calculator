833ff904522e795f23642009817d804b758bfa5e
Prince Goel <princegoel072@gmail.com>
2021-03-10 09:48:36 +0000

    Final Project

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ function slidervalue() {
 
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal");
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    
+    if (principal.value > 0) {
+        result = document.getElementById("result");
+        rAmount = document.getElementById("rAmount");
+        rRate = document.getElementById("rRate");
+        rResult = document.getElementById("rResult");
+        rYear = document.getElementById("rYear");
+        var today = new Date();
+        var year = today.getFullYear();
+
+        rAmount.innerText = principal.value;
+        rRate.innerText = rate + ' %';
+        rResult.innerText = (principal.value * rate * years) /100;
+        rYear.innerText = parseInt(year) + parseInt(years);
+        
+        result.style.display = 'block';
+    } else {
+        alert("Enter Positive Amount");
+        principal.focus();
+    }
     
 }
         
